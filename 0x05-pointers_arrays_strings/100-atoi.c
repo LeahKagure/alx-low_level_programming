8a61496daae3f8062d3431706082eef92a6c5350
LeahKagure <leah.kagure98@gmail.com>
2022-03-21 06:47:22 -0700

    Great leaders are willing to sacrifice the numbers to save the people

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,42 +1,29 @@
 #include "main.h"
-
 /**
- * _atoi - Convert a string to integer.
- * @s: char array string
- * Description: Number in the string can be preceded by an infinite
- * number of characters.
- * You need to take into account all -/+ signs before the number.
- * If there are no numbers in the string, return 0.
- * No need to check for overflow.
- * Not allowed to hard-code special values.
- * Return: first integer found in string
+ * _atoi - int
+ * @s: pointer
+ * Return: int.
  */
-
 int _atoi(char *s)
 {
 	int i;
-	int h, p;
+	int res = 0;
+	int sig = -1;
+	int brk = 0;
 
-	h = 0;
-	p = -1;
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == '-')
-			p *= -1;
-
-		if (s[i] > 47 && s[i] < 58)
+			sig = sig * -1;
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			if (h < 0)
-				h = (h * 10) - (s[i] - '0');
-			else
-				h = (s[i] - '0') * -1;
-
-			if (s[i + 1] < 48 || s[i + 1] > 57)
-				break;
+			res = res * 10;
+			res -= (s[i] - '0');
+			brk = 1;
 		}
+		else if (brk == 1)
+			break;
 	}
-	if (p < 0)
-		h *= -1;
-
-	return (h);
+	res = sig * res;
+	return (res);
 }
