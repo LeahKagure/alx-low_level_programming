56b5a5f0f46b9d089142f940de1e60fb1baa59ab
LeahKagure <leah.kagure98@gmail.com>
2022-06-19 04:47:44 -0700

    Code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/LeahKagure/alx-low_level_programming/tree/main/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+gcc *.o -shared -o 4libc.so
+LD_PRELOAD=$PWD/4libc.so
